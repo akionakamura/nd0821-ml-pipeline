5d22138ccee29c700c8d4b11ff41c8d849435a0d
Akio Nakamura <akionakas@gmail.com>
2021-09-27 23:59:47 -0300

    Add train_random_forest and test_regresstion_model

--- main.py
+++ main.py
@@ -85,35 +85,43 @@ def go(config: DictConfig):
                 "main",
                 parameters={
                     "input": "clean_sample.csv:latest",
-                    "test_size": config["data_split"]["test_size"],
-                    "random_seed": config["data_split"]["random_seed"],
-                    "stratify_by": config["data_split"]["stratify_by"]
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
                 },
             )
 
         if "train_random_forest" in active_steps:
-
+            # Trains Random Forest.
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "clean_sample.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                },
+            )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            # Run regression test against the prod model with test data.
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
 
 
 if __name__ == "__main__":
